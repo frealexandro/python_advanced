bff05fc7417472a40565ee4fe553058063c6b2fb
frealexandro <jhoner300@gmail.com>
2022-03-17 12:30:09 -0500

    no imprime el mensaje

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+""""python module palindrome """
+
+def is_palindrome (string:str)-> bool:
+    string = string.replace(" " , "").lower()
+    return string == string [::-1]
+
+
+def run():
+    print(is_palindrome("ana"))
+
+if __name__== '__main__':
+    run 
