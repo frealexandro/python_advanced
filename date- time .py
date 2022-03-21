4f2bb72eeea6125bab6ead751363373e7a15fe09
Jhoner Santiago Novoa Rojas <jhoner300@gmail.com>
2022-03-21 12:51:13 -0500

    Update date-time.py

--- date- time .py
+++ date- time .py
@@ -1,1 +1,14 @@
 
+from datetime import datetime 
+import pytz
+
+mexico_timezone = pytz.timezone("America/Mexico_City")
+mexico_date = datetime.now(mexico_timezone)
+print("ciudad de mexico:",mexico_date.strftime("%d/%m/%Y ,%H : %M : %s "))
+
+from datetime import datetime 
+import pytz
+
+bogota_timezone = pytz.timezone("America/Bogota")
+bogota_date = datetime.now(bogota_timezone)
+print("bogota:",bogota_date.strftime("%d/%m/%Y ,%H : %M : %s "))
